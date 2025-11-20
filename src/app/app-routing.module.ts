cf5d49310efebb4ea3ab17e976e1bee0c76c69f7
JIEUN LEE <jieun2@illinois.edu>
2025-11-19 22:20:17 -0500

    update_111925

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ResearchInfoComponent } from './research-info/research-info.component';
-import { ProfileComponent } from './profile/profile.component';
-import { PublicationsComponent } from './publications/publications.component'
+// import { ProfileComponent } from './profile/profile.component';
+// import { PublicationsComponent } from './publications/publications.component'
 import { TeachingComponent } from './teaching/teaching.component';
 //import { RecommendationsComponent } from './recommendations/recommendations.component';
 // import { FilesComponent } from './files/files.component';
@@ -12,8 +12,8 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'research', component: ResearchInfoComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'publications', component: PublicationsComponent },
+  // { path: 'profile', component: ProfileComponent },
+  // { path: 'publications', component: PublicationsComponent },
   { path: 'teaching', component: TeachingComponent },
 //  { path: 'recommendation', component: RecommendationsComponent },
   // { path: 'files', component: FilesComponent },
